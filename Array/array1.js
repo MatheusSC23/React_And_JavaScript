d554d332194a2b7a6adb775b81cfeead2932a8f5
Lukiam23 <mscorreia95@gmail.com>
2021-10-30 23:45:36 -0300

    First Array Lessons

--- Array/array1.js
+++ Array/array1.js
@@ -8,3 +8,11 @@ console.log(aprovados[0])
 console.log(aprovados[1])
 console.log(aprovados[2])
 console.log(aprovados[3])
+
+aprovados[3] = 'Paulo'
+console.log(aprovados[3])
+aprovados.push('Abia')
+console.log(aprovados.length)
+
+aprovados[9] = 'Rafael'
+console.log(aprovados)
