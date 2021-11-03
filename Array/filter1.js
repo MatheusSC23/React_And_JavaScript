6812018cb3b6af4cea5f5ea0f1cd5e899d77588b
Lukiam23 <mscorreia95@gmail.com>
2021-11-02 23:48:28 -0300

    Filter class

--- Array/filter1.js
+++ Array/filter1.js
@@ -1,10 +1,10 @@
 const produtos = [
 	{nome: 'notebook', preco: 2499, fragil:true},
-	{nome: 'iPad Pro', preco: 4199, fragil:true},
+	{nome: 'iPad Pro', preco: 4199, fragil:false},
 	{nome: 'Copo de Vidro', preco: 12.49, fragil:true},
 	{nome: 'Copo de plástico', preco: 18.99, fragil:false},
 ]
 
-const eFragil = p => {return p.fragil}
-const eCaro = p => {return p.preco > 2000}
-console.log(produtos.filter(eFragil).filter(eCaro))
+const eFragil = p => p.fragil
+const eCaro = p =>  p.preco >= 500
+console.log(produtos.filter(eCaro).filter(eFragil))
