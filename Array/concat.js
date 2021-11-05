ef4494901993a19ad66643f4acbdb5b04f7c68a1
Lukiam23 <mscorreia95@gmail.com>
2021-11-04 23:45:53 -0300

    concat

---
+++ Array/concat.js
@@ -0,0 +1,8 @@
+const filhas = ['Valeska', 'Sibalena']
+const filhos = ['Pedro', 'João']
+
+const todos = filhos.concat(filhas, 'Guilerme')
+
+console.log(filhos, filhas, todos)
+
+console.log([['a','b']].concat([1,2],[3,4],5,[[6,7]]))
