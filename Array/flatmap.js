0997aa19500258293d6c6df4be0eeb2462f2a7da
Lukiam23 <mscorreia95@gmail.com>
2021-11-04 23:57:02 -0300

    flatmap part2

--- Array/flatmap.js
+++ Array/flatmap.js
@@ -23,3 +23,10 @@ const getNotasDaTurma = turma => turma.alunos.map(getNotaDoAluno)
 
 const notas1 = escola.map(getNotasDaTurma)
 console.log(notas1)
+
+Array.prototype.flatMap = function(callback) {
+	return Array.prototype.concat.apply([],this.map(callback))
+};
+
+const notas2 = escola.flatMap(getNotasDaTurma)
+console.log(notas2)
